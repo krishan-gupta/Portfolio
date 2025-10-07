55db2244a6cd61fb17da9c02f0683ea56842944b
krishan-gupta <krishangupta204@gmail.com>
2025-10-07 22:02:28 +0530

    data changes

--- script.js
+++ script.js
@@ -48,25 +48,6 @@ $(document).ready(function(){
         loop: true
     });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+    // owl carousel script was removed from here as it was causing an error.
+
 });
